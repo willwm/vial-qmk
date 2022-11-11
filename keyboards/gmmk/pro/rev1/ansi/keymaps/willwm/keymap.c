a732d33fc77730e13cd6d202db0c017a72af9097
Will Wolff-Myren <willwm@gmail.com>
2022-11-11 06:27:42 -0800

    gmmk/pro:willwm Add layers enum and FN_* defs

--- keyboards/gmmk/pro/rev1/ansi/keymaps/willwm/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/willwm/keymap.c
@@ -16,37 +16,54 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-#define FN_CAPS LT(3, KC_PSCR)
+enum layers {
+    BASE,
+    FUNC,
+    TOP,
+    FRNT,
+};
+
+#define FN_BASE TO(BASE)
+#define FN_FUNC OSL(FUNC)
+#define FN_TOP  OSL(TOP)
+#define FN_FRNT OSL(FRNT)
+
 
 // Encoders - Encoder Map ================================================= //
 // https://docs.qmk.fm/#/feature_encoders?id=encoder-map                    //
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [2] = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD) },
-    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [BASE]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [FUNC]  = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [TOP]   = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [FRNT]  = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    //      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Del           Rotary(Mute)
-    //      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Home
-    //      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \                 PgUp
-    //      FN_CAPS  A        S        D        F        G        H        J        K        L        ;        "                 Enter             PgDn
-    //      Sh_L              Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
-    //      Ct_L     Alt_L    Win_L                               SPACE                               Win_R    Alt_R    Ct_R     Left     Down     Right
-    [0] = LAYOUT(
+    // Default
+    [BASE] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        FN_FUNC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
-    [1] = LAYOUT(
+    // Function
+    [FUNC] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_RBT,            KC_PSCR,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,           FN_TOP,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,           FN_FRNT,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,  FN_BASE,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______,  _______
+    ),
+
+    // Top
+    [TOP] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
@@ -55,36 +72,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
 
-    [2] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           RGB_TOG,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           RGB_SPI,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           RGB_VAI,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,           RGB_VAD,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, RGB_SAI,  RGB_SPD,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_HUD, RGB_SAD,  RGB_HUI
+    // FRNT
+    [FRNT] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
 
-    [3] = LAYOUT(
-        QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,            _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           KC_PSCR,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,           _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,           _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_CAPS, _______,  _______,
-        _______, _______, _______,                            KC_PSCR,                            _______, _______, _______, _______, _______,  _______
-    ),
+    
 };
 // clang-format on
-
-// Caps Word - Representing Caps Word state =============================== //
-// https://docs.qmk.fm/#/feature_caps_word?id=representing-caps-word-state  //
-
-void caps_word_set_user(bool active) {
-    if (active) {
-        // Do something when Caps Word activates.
-    } else {
-        // Do something when Caps Word deactivates.
-    }
-}
 
 // RGB Matrix - Callbacks ================================================= //
 // https://docs.qmk.fm/#/feature_rgb_matrix?id=callbacks                    //
@@ -103,23 +103,20 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 void rgb_matrix_indicators_user(void) {
     // Show Caps Word state
     if (is_caps_word_on()) {
-        rgb_matrix_set_color_all(RGB_RED);
+        rgb_matrix_set_color_all(RGB_PURPLE);
     }
 
     // Show active layear
     uint8_t layer = get_highest_layer(layer_state|default_layer_state);
 
     switch(layer) {
-        case 4:
-            rgb_matrix_set_color_all(RGB_WHITE);
-            break;
-        case 3:
+        case FRNT:
             rgb_matrix_set_color_all(RGB_CYAN);
             break;
-        case 2:
+        case TOP:
             rgb_matrix_set_color_all(RGB_ORANGE);
             break;
-        case 1:
+        case FUNC:
             rgb_matrix_set_color_all(RGB_CHARTREUSE);
             break;
         default:
